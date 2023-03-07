093605f02cebd44c91a52d6c50d0ddc7cae5a92a
fernando <fernandogonzalezcruz2004@users.noreply.github.com>
2023-03-06 23:18:32 -0600

    worked on exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let sol = ""
+    if(num < 0){
+        return "ERROR"
+    }
+    for (let index = 0; index < num; index++) {
+        sol = String(sol + string)
+      
+        
+    }
+    return sol
 };
 
 // Do not edit below this line
